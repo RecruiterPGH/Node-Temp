cb7ff890fce0168881735d63fd85d4d0ab0da4fd
dan smith <XX1011@gmail.com>
2017-01-04 03:59:20 -0500

    number generation and win condition

--- game.js
+++ game.js
@@ -1,11 +1,22 @@
 import readline from 'readline'
 
-const reader = readline.createInterface({
+const min = 1
+    , max = 100
+    , num = Math.floor(Math.random() * (max - min + 1)) + min
+    , prompt = readline.createInterface({
 	input:  process.stdin,
 	output: process.stdout
 })
+    , print = ::console.log
 
-reader.question('? ', (guess) => {
-	console.log('you guessed ' + guess)
-	reader.close()
-})
+print(`I'm thinking of a number between ${min} and ${max}`)
+
+(function loop() {
+	prompt.question('? ', (guess) => {
+		if (guess == num) {
+			print('correct!')
+			prompt.close()
+		}
+		prompt.close()
+	})
+}())
