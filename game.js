e9f9fd84d9cf597744cc004b38ec89ecf441e62f
dan smith <XX1011@gmail.com>
2017-01-04 05:09:02 -0500

    xo

--- game.js
+++ game.js
@@ -3,17 +3,17 @@ import readline from 'readline'
 const min = 1
     , max = 100
     , num = Math.floor(Math.random() * (max - min + 1)) + min
-    , prompt = readline.createInterface({
+    , print = ::console.log
+const prompt = readline.createInterface({
 	input:  process.stdin,
 	output: process.stdout
 })
-    , print = ::console.log
 
 print(`I'm thinking of a number between ${min} and ${max}`)
 
-(function loop() {
-	prompt.question('? ', (guess) => {
-		if (guess == num) {
+;(function loop() {
+	prompt.question('? ', guess => {
+		if (guess === num) {
 			print('correct!')
 			prompt.close()
 		}
