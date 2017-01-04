b0511cd71f1d9d22534b417a09fb1c509f0355cc
dan smith <XX1011@gmail.com>
2017-01-03 22:50:44 -0500

    try to promisify readline's question()
    didn't work because question() doesn't follow the node convention of passing err as the 1st argument to the callback

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-console.log('hi!')
+import readline from 'readline'
+import Promise from 'bluebird'
+
+const reader = readline.createInterface({
+	input:  process.stdin,
+	output: process.stdout
+})
+const question = Promise.promisify(reader.question)
+
+(async () => {
+	let guess = await question('?')
+})()
