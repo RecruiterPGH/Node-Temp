a6d3ddccce56180000627715b3246fb89504b355
dan smith <XX1011@gmail.com>
2017-01-03 22:57:44 -0500

    got basic I/O working using plain callback

--- game.js
+++ game.js
@@ -1,12 +1,11 @@
 import readline from 'readline'
-import Promise from 'bluebird'
 
 const reader = readline.createInterface({
 	input:  process.stdin,
 	output: process.stdout
 })
-const question = Promise.promisify(reader.question)
 
-(async () => {
-	let guess = await question('?')
-})()
+reader.question('? ', (guess) => {
+	console.log('you guessed ' + guess)
+	reader.close()
+})
